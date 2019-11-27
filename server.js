f00769c4816e81609adb18eaab40a42cf8cc635b
Kiryl_Mudrakou <Kiryl_Mudrakou@epam.com>
2019-11-27 20:59:48 +0300

    express

--- server.js
+++ server.js
@@ -12,17 +12,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/news', (req, res, next) => {
-  res.send(news);
+  if (req.url === '/news') {
+    res.send(news);
+  } else {
+    next(new Error('not valid url'));
+  }
 });
 
-app.get('/news/:id', (req, res) => {
-    let user = users.find(user => {
-      return user.id === req.params.id;
+app.get('/news/:id', (req, res, next) => {
+  if (req.url === '/news/:id') {
+    let getNews = news.find(newss => {
+      return newss.id === req.params.id;
     });
-    res.send(user);
-});
+    res.send(getNews);
+  } else {
+    next(new Error('not valid url'));
+  }
+})
+;
 
 app.put('/news/:id', (req, res) => {
+  if (req.url === '/news/:id') {
     let putNews = news.find(putNewsId => {
       return putNewsId.id === req.params.id;
     });
@@ -30,10 +40,14 @@ app.put('/news/:id', (req, res) => {
     putNews.title = req.body.title;
     res.sendStatus(200);
     console.log('done put');
+  } else {
+    next(new Error('not valid url'));
+  }
 });
 
 app.post('/news/add', (req, res) => {
-     let addNews = {
+  if (req.url === '/news/add') {
+    let addNews = {
       id: req.body.id,
       author: req.body.author,
       source: req.body.source,
@@ -45,12 +59,19 @@ app.post('/news/add', (req, res) => {
     news.push(addNews);
     res.sendStatus(200);
     console.log('done post');
+  } else {
+    next(new Error('not valid url'));
+  }
 });
 
 app.delete('/news/:id', (req, res) => {
-  return deleteNews.id !== req.params.id;
-  res.sendStatus(200);
-  console.log('done delete');
+  if (req.url === '/news/:id') {
+    return deleteNews.id !== req.params.id;
+    res.sendStatus(200);
+    console.log('done delete');
+  } else {
+    next(new Error('not valid url'));
+  }
 
 });
 
