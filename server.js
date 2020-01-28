7826f7c48b09f83965310d0093e920a9654bac00
Kiryl_Mudrakou <Kiryl_Mudrakou@epam.com>
2020-01-28 17:02:55 +0300

    fixed comments

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
+const Schema = mongoose.Schema;
+
+const url = 'mongodb+srv://kirillz732:admin@cluster0-km53s.mongodb.net/news?retryWrites=true&w=majority';
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(() => console.log('Db connected'))
+  .catch(err => console.log('error'));
+
+const allNews = new Schema({
+  id: String,
+  author: String,
+  source: String,
+  title: String,
+  content: String,
+  publishedAt: String
+});
+
+const News = mongoose.model('News', allNews);
 
 const app = express();
 
-let news = require('./news.json');
+//let news = require('./news.json');
 
 const port = 8000;
 
@@ -13,41 +32,51 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/news', (req, res, next) => {
   if (req.url === '/news') {
-    res.send(news);
+    News.find().then(news => res.send(news));
   } else {
     next(new Error('not valid url'));
   }
 });
 
 app.get('/news/:id', (req, res) => {
-  let getNews = news.find(newss => {
-    return newss.id === req.params.id;
-  });
-  res.send(getNews);
+  // let getNews = news.find(newss => {
+  //   return newss.id === req.params.id;
+  // });
+  //res.send(getNews);
+  News.find({'id': req.params.id}).then(news => res.send(news));
 })
 ;
 
 app.put('/news/:id', (req, res) => {
-  let putNews = news.find(putNewsId => {
-    return putNewsId.id === req.params.id;
-  });
-  putNews.author = req.body.author;
-  putNews.title = req.body.title;
+  // let putNews = news.find(putNewsId => {
+  //   return putNewsId.id === req.params.id;
+  // });
+  // putNews.author = req.body.author;
+  // putNews.title = req.body.title;
+  News.update({'id' : req.params.id}, {$set: {'author' : req.body.author, 'title': req.body.title}}).then(news => res.send(news));
   res.sendStatus(200);
   console.log('done put');
 });
 
 app.post('/news/add', (req, res, next) => {
   if (req.url === '/news/add') {
-    let addNews = {
+    // let addNews = {
+    //   id: req.body.id,
+    //   author: req.body.author,
+    //   source: req.body.source,
+    //   title: req.body.title,
+    //   content: req.body.content,
+    //   publishedAt: req.body.publishedAt
+    // };
+    // news.push(addNews);
+    News.create( {
       id: req.body.id,
       author: req.body.author,
       source: req.body.source,
-      title: req.body.title,
+      title:  req.body.title,
       content: req.body.content,
-      publishedAt: req.body.publishedAt
-    };
-    news.push(addNews);
+      publishedAt: req.body.publishedA
+    })
     res.sendStatus(200);
     console.log('done post');
   } else {
@@ -56,9 +85,10 @@ app.post('/news/add', (req, res, next) => {
 });
 
 app.delete('/news/:id', (req, res) => {
-  news = news.filter(deleteNews => {
-    return deleteNews.id !== req.params.id;
-  });
+  // news = news.filter(deleteNews => {
+  //   return deleteNews.id !== req.params.id;
+  // });
+  News.remove({'id': req.params.id}).then(news => res.send(news));
   res.sendStatus(200);
   console.log('done delete');
 });
